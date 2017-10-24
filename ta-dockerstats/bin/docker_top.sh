2ed200daf212bb1c59d1148824e243c7abb84e6c
Marc Chene <mchene@splunk.com>
2017-10-24 15:54:13 -0700

    Update docker top arguments and formating that was causing errors with latest docker client.

--- ta-dockerstats/bin/docker_top.sh
+++ ta-dockerstats/bin/docker_top.sh
@@ -4,9 +4,11 @@ DOCKER_BIN=$(dirname "$0")/docker
 for container_id in $("$DOCKER_BIN" ps -q); do
   command_date=$(date -u +%Y-%m-%dT%H:%M:%S%z)
   "$DOCKER_BIN" top $container_id \
-    -Ao pid,ppid,pgid,pcpu,vsz,nice,etime,time,tty,ruser,user,rgroup,group,comm,args:1000 | \
+    -Ao pid,ppid,pgid,vsz,tty,user,group,comm,args | \
     tail -n +2 | \
     sed -E 's/"/""/g' | \
-    sed -E 's/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/' | \
+    sed -E 's/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/' | \
     sed -E "s/^(.*)$/\"$command_date\",\"$container_id\",\"\1\"/"
 done
+#    -Ao pid,ppid,pgid,pcpu,vsz,nice,etime,time,tty,ruser,user,rgroup,group,comm,args:1000 | \
+#    sed -E 's/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/; s/  +/","/' | \
