2c71188b4a00f9571d687dc69438fc5e36fc20a9
Marc Chene <mchene@splunk.com>
2018-01-03 12:08:08 -0800

    Updated k8s app folder for app-k8s (includes changings dashboard drilldowns). Using gtar for packaging k8s app as SPL.

--- build-k8s-app-spl.sh
+++ build-k8s-app-spl.sh
@@ -1,3 +1,4 @@
-tar -zcvf ./app-k8s.tar.gz ./k8s/
-mv ./app-k8s.tar.gz ./app-k8s.spl
-
+# Build for running on Mac OsX, using gtar to prevent untarring issues on linux, https://superuser.com/questions/318809/linux-os-x-tar-incompatibility-tarballs-created-on-os-x-give-errors-when-unt
+# using GNU tar
+gtar -zcvf ./app-k8s.tar ./app-k8s/
+mv ./app-k8s.tar ./app-k8s.spl 
